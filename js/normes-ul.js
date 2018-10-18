e43e9ab648dda76f21bb214f205ed8bc43c9cb91
Jean-Philippe Lévesque <jean-philippe.levesque@bsw.ulaval.ca>
2018-10-18 11:49:50 -0400

    Révision de code - Indentation

--- js/normes-ul.js
+++ js/normes-ul.js
@@ -96,9 +96,8 @@ function toggleRecherche(){
     jQuery('#section-recherche input').focus();
   } else {
     jQuery('#section-recherche').animate({height: 14}, 100, 'swing', function() {
-    jQuery('#section-recherche').hide()
-    jQuery('#section-recherche').height(hauteurStrate);
-      });
-    }
+      jQuery('#section-recherche').hide()
+      jQuery('#section-recherche').height(hauteurStrate);
+    });
   }
-  
+}
